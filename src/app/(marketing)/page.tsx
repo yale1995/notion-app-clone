261186c6517ef7d855c456af7045a5af3d45a933
Yale Araujo <yale850@gmail.com>
2024-01-02 14:24:52 -0300

    feat: marketing landing page

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+import { Heading } from './_components/heading'
+
+export default function MarketingPage() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+      </div>
+    </div>
+  )
+}
