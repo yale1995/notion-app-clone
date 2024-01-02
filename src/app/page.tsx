2ce9dda65bf38029e3c721ed88ceaca0c07f292c
Yale Araujo <yale850@gmail.com>
2024-01-02 11:10:36 -0300

    feat: shadcn-ui button test in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1>Homepage</h1>
+import { Button } from '@/components/ui/button'
+
+export default function Homepage() {
+  return (
+    <div className="mx-auto text-center">
+      <h1>Yale Henrique</h1>
+      <Button variant="default" size="icon">
+        Click me
+      </Button>
+    </div>
+  )
 }
