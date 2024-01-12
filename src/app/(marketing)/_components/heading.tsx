0296227f90e133e4fe963eb65896a835fba5b507
Yale Araujo <yale850@gmail.com>
2024-01-12 00:14:35 -0300

    feat: added authentication and unauthentication buttons

--- src/app/(marketing)/_components/heading.tsx
+++ src/app/(marketing)/_components/heading.tsx
@@ -1,9 +1,16 @@
 'use client'
 
+import Link from 'next/link'
+
+import { Spinner } from '@/components/spinner'
 import { Button } from '@/components/ui/button'
+import { useConvexAuth } from 'convex/react'
 import { ArrowRight } from 'lucide-react'
+import { SignInButton } from '@clerk/clerk-react'
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -16,10 +23,28 @@ export const Heading = () => {
         better, faster, work happens.
       </h3>
 
-      <Button>
-        Entrer Jotion
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="flex items-center justify-center">
+          {' '}
+          <Spinner />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Entrer Jotion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+
+      {!isLoading && !isAuthenticated && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Jotion Free <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   )
 }
--- src/app/(marketing)/_components/navbar.tsx
+++ src/app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,19 @@
 'use client'
 
+import Link from 'next/link'
+
 import { useScrollTop } from '@/hooks/use-scroll-top'
 import { cn } from '@/lib/utils'
 import { Logo } from './logo'
 import { ThemeToggle } from '@/components/theme-toggle'
+import { useConvexAuth } from 'convex/react'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { Button } from '@/components/ui/button'
+import { Fragment } from 'react'
+import { Spinner } from '@/components/spinner'
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
   const scrolled = useScrollTop()
 
   return (
@@ -17,7 +25,32 @@ export const Navbar = () => {
     >
       <Logo />
 
-      <div className="md:ml-auto md:justify-end justify-between w-full flex itemce gap-x-2">
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <Fragment>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Login
+              </Button>
+            </SignInButton>
+
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Get Jotion Free
+              </Button>
+            </SignInButton>
+          </Fragment>
+        )}
+        {isAuthenticated && !isLoading && (
+          <Fragment>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </Fragment>
+        )}
+
         <ThemeToggle />
       </div>
     </div>
