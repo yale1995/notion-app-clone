4d3f0045810294d3345d9eba836fa1e6391b9394
Yale Araujo <yale850@gmail.com>
2024-01-12 10:06:32 -0300

    feat: main layout with authentication verification

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Spinner } from '@/components/spinner'
+import { useConvexAuth } from 'convex/react'
+import { redirect } from 'next/navigation'
+import { ReactNode } from 'react'
+import { Navigation } from './_components/navigation'
+
+interface MainLayoutProps {
+  children: ReactNode
+}
+export default function MainLayout({ children }: MainLayoutProps) {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Spinner size="lg" />
+      </div>
+    )
+  }
+
+  if (!isAuthenticated) {
+    return redirect('/')
+  }
+  return (
+    <div className="h-screen flex dark:bg-[#1f1f1f]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">{children}</main>
+    </div>
+  )
+}
