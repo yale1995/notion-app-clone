54ace21de00184b6292a2cd7c18498a3b769da8c
Yale Araujo <yale850@gmail.com>
2024-01-12 00:12:28 -0300

    chore: convex files setup

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+
+export default {
+  providers: [
+    {
+      domain: "https://true-bluejay-33.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
