8343f5266d2f1e5a5e4ba8714407955a2352fbe9
Yale Araujo <yale850@gmail.com>
2024-01-13 00:16:08 -0300

    refactor: added sonner into layout wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 import { Inter } from 'next/font/google'
+import { Toaster } from 'sonner'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexProvider } from '@/components/providers/convex-provider'
 
@@ -43,6 +44,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="jotion-theme"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexProvider>
