d489e7bf7ee0e8d3d77bcdd10be995d372cf6bd4
Yale Araujo <yale850@gmail.com>
2024-02-04 02:06:58 -0300

    refactor: merging server and client envs and applying safeParse zod method

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,30 @@
 import { z } from 'zod'
 
-const envSchema = z.object({
+const server = z.object({
   CONVEX_DEPLOYMENT: z.string(),
   CLERK_SECRET_KEY: z.string(),
 })
 
-const parsedEnv = envSchema.safeParse(process.env)
+const client = z.object({
+  NEXT_PUBLIC_CONVEX_URL: z.string().url(),
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+})
+
+const processEnv = {
+  CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+}
+
+const merged = server.merge(client)
+
+const isServer = typeof window === 'undefined'
+
+const parsedEnv = isServer
+  ? merged.safeParse(processEnv) // --> on serve it is possible to validate all variables
+  : client.safeParse(processEnv) // --> on client only variables exposed to the browser
 
 if (!parsedEnv.success) {
   console.error(
