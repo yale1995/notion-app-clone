0fb77b60bdf0f706542eb39e4363f9648b8df84d
Yale Araujo <yale850@gmail.com>
2024-02-05 16:13:58 -0300

    feat: new method created getSearch

--- convex/documents.ts
+++ convex/documents.ts
@@ -2,6 +2,27 @@ import { v } from 'convex/values'
 import { Doc, Id } from './_generated/dataModel'
 import { mutation, query } from './_generated/server'
 
+export const getSearch = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity()
+
+    if (!identity) {
+      throw new Error('Not authenticated')
+    }
+
+    const userId = identity.subject
+
+    const documents = await ctx.db
+      .query('documents')
+      .withIndex('by_user', (q) => q.eq('userId', userId))
+      .filter((q) => q.eq(q.field('isArchived'), false))
+      .order('desc')
+      .collect()
+
+    return documents
+  },
+})
+
 export const remove = mutation({
   args: { id: v.id('documents') },
   handler: async (ctx, args) => {
